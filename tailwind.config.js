5ccc2de092c5739a4160fb30c5caa5a71cb2bd12
yoohaso <solhanyun@gmail.com>
2025-04-06 15:49:00 +0900

    feat: add Header in Home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'common-home-searchbar': '#805AF1',
+        'common-home-header-background': '#FFFFFF',
+        'common-container-presentCard': '#FFFFFF',
+        'common-tabbar': '#FFFFFFE5',
+        'common-search': '#FFFFFF',
+        'form-button-primary': '#C9B8FF',
+        'text-default': '#000000',
+        'base-grayscale-grayscale-0': '#000000',
+      },
+      borderRadius: {
+        'common-border-radius-full': '1000px',
+      },
+      gap: {
+        'unit-sx': '12px',
+      },
+    },
   },
   plugins: [],
 };
