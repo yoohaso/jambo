bb0c4a227f19d808119f249e3e96a65953d9cad0
yoohaso <solhanyun@gmail.com>
2025-04-06 23:41:03 +0900

    feat: add paging with infinite scroll

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,19 +1,63 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { PostCard, VideoCard } from './Card';
-import { fetchCards } from '@/services/card/function';
+import { fetcCardsWithPage, fetchCards } from '@/services/card/function';
 import { CardItem } from '@/services/card/type';
+
+type IntersectHandler = (entry: IntersectionObserverEntry, observer: IntersectionObserver) => void;
+
+const useIntersect = (onIntersect: IntersectHandler, options?: IntersectionObserverInit) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const callback = useCallback(
+    (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          onIntersect(entry, observer);
+        }
+      });
+    },
+    [onIntersect]
+  );
+
+  useEffect(() => {
+    if (!ref.current) {
+      return;
+    }
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(ref.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref, options, callback]);
+
+  return ref;
+};
 
 export function CardList() {
   const [cards, setCards] = useState<CardItem[]>([]);
+  const [hasNext, setHasNext] = useState(false);
+
+  const lastCardId = cards.length > 0 && cards[cards.length - 1].card.cardId;
 
   useEffect(() => {
     const getCards = async () => {
-      const { list } = await fetchCards();
+      const { list, hasNext } = await fetchCards();
       setCards(list);
+      setHasNext(hasNext);
     };
 
     getCards();
   }, []);
+
+  const ref = useIntersect(async (entry, observer) => {
+    observer.unobserve(entry.target);
+
+    if (hasNext && lastCardId) {
+      const { list, hasNext } = await fetcCardsWithPage(lastCardId);
+      setCards(prev => [...prev, ...list]);
+      setHasNext(hasNext);
+    }
+  });
 
   return (
     <div className="flex flex-col items-center gap-common-presentCard-gap p-[8px]">
@@ -50,6 +94,7 @@ export function CardList() {
           />
         );
       })}
+      <div ref={ref}></div>
     </div>
   );
 }
--- src/services/card/function.ts
+++ src/services/card/function.ts
@@ -5,3 +5,8 @@ export const fetchCards = async () => {
   const response = await ky.get<CardResponse>('/api/v1/card').json();
   return response;
 };
+
+export const fetcCardsWithPage = async (page: string) => {
+  const response = await ky.get<CardResponse>(`/api/v1/card?page=${page}`).json();
+  return response;
+};
