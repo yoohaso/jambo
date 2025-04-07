5ac91ad2426faf3016f4d670dd4c1d113a64836b
yoohaso <solhanyun@gmail.com>
2025-04-07 15:29:27 +0900

    refactor: modify tabbar icon naming and extract icons into seperate files

---
+++ src/components/icons/index.ts
@@ -0,0 +1,5 @@
+export { HomeIcon } from './HomeIcon';
+export { PlusIcon } from './PlusIcon';
+export { UnionIcon } from './UnionIcon';
+export { SayIcon } from './SayIcon';
+export { MyIcon } from './MyIcon';
