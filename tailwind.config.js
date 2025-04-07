89c67afa8d6f51e19d746abf2934b515b2686c94
yoohaso <solhanyun@gmail.com>
2025-04-07 15:14:13 +0900

    feat: add tabbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ export default {
         'text-default': '#000000',
         'base-grayscale-grayscale-0': '#000000',
         'color-base-white-white': '#FFFFFF',
+        'form-button-primary': '#C9B8FF',
       },
       borderRadius: {
         'common-border-radius-full': '1000px',
@@ -24,6 +25,9 @@ export default {
       },
       padding: {
         'common-padding-presentCard': '20px',
+      },
+      borderColor: {
+        'base-grayscale-grayscale-0': '#000000',
       },
     },
   },
